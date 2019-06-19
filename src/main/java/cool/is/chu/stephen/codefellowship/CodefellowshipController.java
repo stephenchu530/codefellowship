268b8d36cbb601f2933f0f143ae3bbe891fb15f7
STEPHEN CHU <stephen.chu530@gmail.com>
2019-06-19 08:24:06 -0700

    fix: added features as required

--- src/main/java/cool/is/chu/stephen/codefellowship/CodefellowshipController.java
+++ src/main/java/cool/is/chu/stephen/codefellowship/CodefellowshipController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.security.Principal;
 
 @Controller
@@ -16,9 +14,11 @@ public class CodefellowshipController {
 
     @GetMapping("/")
     public String getCodefellowship(Principal p, Model m) {
-        if (p != null)
-            m.addAttribute("user", p.getName());
-            m.addAttribute("present", p);
+        if (p != null) {
+            AppUser user = appUserRepository.findByUsername(p.getName());
+            m.addAttribute("user", user);
+        }
+        m.addAttribute("p", p);
         return "codefellowship";
     }
 }
